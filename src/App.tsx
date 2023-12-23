3c9a8580ce19d95ea04cd1e8d919790332e013aa
Roberto Costa <roberto.araujo339@gmail.com>
2023-12-23 19:45:39 -0300

    Update ESLint and Prettier configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,44 @@
-import { fetchFile } from "@ffmpeg/util";
-import { useEffect, useRef, useState } from "react";
-import { ffmpegClient, loadFfmpeg } from "./services/ffmpeg";
+import { fetchFile } from '@ffmpeg/util'
+import { useEffect, useRef, useState } from 'react'
+import { ffmpegClient, loadFfmpeg } from './services/ffmpeg'
 
 function App() {
-  const videoRef = useRef<HTMLVideoElement | null>(null);
-  const [log, setLog] = useState("");
+  const videoRef = useRef<HTMLVideoElement | null>(null)
+  const [log, setLog] = useState('')
 
   const transcode = async () => {
-    await loadFfmpeg();
+    await loadFfmpeg()
 
-    const videoURL = "http://localhost:5173/bunny.webm";
+    const videoURL = 'http://localhost:5173/bunny.webm'
 
-    const ffmpeg = ffmpegClient;
+    const ffmpeg = ffmpegClient
 
-    await ffmpeg.writeFile("input.webm", await fetchFile(videoURL));
+    await ffmpeg.writeFile('input.webm', await fetchFile(videoURL))
 
-    await ffmpeg.exec(["-i", "input.webm", "output.mp4"]);
+    await ffmpeg.exec(['-i', 'input.webm', 'output.mp4'])
 
-    const fileData = await ffmpeg.readFile("output.mp4");
+    const fileData = await ffmpeg.readFile('output.mp4')
 
-    const data = new Uint8Array(fileData as ArrayBuffer);
+    const data = new Uint8Array(fileData as ArrayBuffer)
 
     if (videoRef.current) {
-      videoRef.current.style.display = "block";
+      videoRef.current.style.display = 'block'
 
       videoRef.current.src = URL.createObjectURL(
-        new Blob([data.buffer], { type: "video/mp4" })
-      );
+        new Blob([data.buffer], { type: 'video/mp4' })
+      )
     }
-  };
+  }
 
   useEffect(() => {
-    ffmpegClient.on("log", ({ message }) => {
-      setLog(message);
-    });
+    ffmpegClient.on('log', ({ message }) => {
+      setLog(message)
+    })
 
     // ffmpegClient.on('progress', ({ progress, time }) => {
     //   console.log('progress, time', progress, time);
     // });
-  }, []);
+  }, [])
 
   return (
     <>
@@ -47,7 +47,7 @@ function App() {
         controls
         width="800px"
         style={{
-          display: "none",
+          display: 'none',
         }}
       ></video>
 
@@ -55,7 +55,7 @@ function App() {
       <button onClick={transcode}>Transcode</button>
       <p>{log}</p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
