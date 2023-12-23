a3e88e079bc7708a00040008ed63ba8b55f91e86
Roberto Costa <roberto.araujo339@gmail.com>
2023-12-23 19:31:53 -0300

    Add editor configuration and npmrc files, update package.json and README.md

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,37 @@
-import { fetchFile } from '@ffmpeg/util';
-import { useEffect, useRef, useState } from 'react';
-import { ffmpegClient, loadFfmpeg } from './services/ffmpeg';
+import { fetchFile } from "@ffmpeg/util";
+import { useEffect, useRef, useState } from "react";
+import { ffmpegClient, loadFfmpeg } from "./services/ffmpeg";
 
 function App() {
   const videoRef = useRef<HTMLVideoElement | null>(null);
-  const [log, setLog] = useState('');
+  const [log, setLog] = useState("");
 
   const transcode = async () => {
     await loadFfmpeg();
 
-    const videoURL = 'http://localhost:5173/bunny.webm';
+    const videoURL = "http://localhost:5173/bunny.webm";
 
     const ffmpeg = ffmpegClient;
 
-    await ffmpeg.writeFile('input.webm', await fetchFile(videoURL));
+    await ffmpeg.writeFile("input.webm", await fetchFile(videoURL));
 
-    await ffmpeg.exec(['-i', 'input.webm', 'output.mp4']);
+    await ffmpeg.exec(["-i", "input.webm", "output.mp4"]);
 
-    const fileData = await ffmpeg.readFile('output.mp4');
+    const fileData = await ffmpeg.readFile("output.mp4");
 
     const data = new Uint8Array(fileData as ArrayBuffer);
 
     if (videoRef.current) {
-      videoRef.current.style.display = 'block';
+      videoRef.current.style.display = "block";
 
       videoRef.current.src = URL.createObjectURL(
-        new Blob([data.buffer], { type: 'video/mp4' })
+        new Blob([data.buffer], { type: "video/mp4" })
       );
     }
   };
 
   useEffect(() => {
-    ffmpegClient.on('log', ({ message }) => {
+    ffmpegClient.on("log", ({ message }) => {
       setLog(message);
     });
 
@@ -45,9 +45,9 @@ function App() {
       <video
         ref={videoRef}
         controls
-        width='800px'
+        width="800px"
         style={{
-          display: 'none',
+          display: "none",
         }}
       ></video>
 
